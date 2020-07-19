0c7526a7a9ac739db98b394c3e0bad237bdd7ec6
Veer Gadodia <veergadodia24@gmail.com>
2020-07-19 10:19:47 -0400

    get help working

--- app/main/nearby_therapists.py
+++ app/main/nearby_therapists.py
@@ -11,15 +11,8 @@ def nearby(zip_code):
     with urllib.request.urlopen('https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q={}&facet=state&facet=timezone&facet=dst'.format(zip_code)) as response:
         loc = json.loads(response.read())
         lat, lon = loc['records'][0]['fields']['geopoint']
-        # print(lat, lon)
 
-    # with urllib.request.urlopen('https://maps.googleapis.com/maps/api/place/nearbysearch/output?key={}&location={},{}&radius={}&keyword={}'.format(zip_code, lat, lon, 5000, "therapists")) as response:
-    #     loc = json.loads(response.read())
-    #     print(loc)
-
-    # url = "https://maps.googleapis.com/maps/api/place/nearbysearch/output?key={}&location={},{}&radius={}&keyword={}".format(API_KEY, lat, lon, 5000, "therapists")
-    url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query={}&key={}&location={},{}&radius={}".format("therapists near me", API_KEY, lat, lon, "2000" )
-    # print(url)
+    url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query={}&key={}&location={},{}&radius={}".format("therapists", API_KEY, lat, lon, "4000" )
 
     r = requests.get(url)
     r = json.loads(r.text)
@@ -29,4 +22,4 @@ def nearby(zip_code):
     return(results)
 
 
-# nearby("02459")
+# pprint(nearby("94301"))
